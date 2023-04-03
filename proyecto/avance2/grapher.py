2b10111f8f82f586a1d1bf44231b3a4f71baaea2
Leobardo Luis Argüelles <66451459+LeobardoArguelles@users.noreply.github.com>
2023-04-02 22:36:40 -0700

    Marcar gráfica de 900 a 1000

--- proyecto/avance2/grapher.py
+++ proyecto/avance2/grapher.py
@@ -13,8 +13,8 @@ data = data[data[:,1].argsort()]
 # Divide the second column by 1000 to get the time in milliseconds
 data[:,1] /= 1000
 
-# Graph the data as a connected line
-plt.plot(data[900:,1], 'k-')
+# Graph the data as a connected line and enumerate the puzzles from 900 to 1000
+plt.plot([x for x in range(900, 1000)], data[900:,1], 'k-')
 
 # Set the x and y axis labels
 plt.xlabel('Puzzle #')
